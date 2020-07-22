35f84203c86d0d817162e4380d967ee3dc5a8529
ashraf-git <aliashraf.hena@gmail.com>
2020-07-22 18:46:49 +0600

    integer-float-parseint-parsefloat

--- hello.js
+++ hello.js
@@ -25,13 +25,32 @@ myname = "ashraf";
 myname = "ashraf";
 console.log(myname);
 
-var promise = "I promise I will work hard to become a Programmer";
-console.log(promise.toLowerCase());
-console.log(promise.toUpperCase())
-console.log(promise.indexOf('will'))
-console.log(promise.indexOf('herd'))
-console.log(promise.split('I'))
-console.log(promise.split(' '))
+// var promise = "I promise I will work hard to become a Programmer";
+// console.log(promise.toLowerCase());
+// console.log(promise.toUpperCase())
+// console.log(promise.indexOf('will'))
+// console.log(promise.indexOf('herd'))
+// console.log(promise.split('I'))
+// console.log(promise.split(' '))
+
+var number1 = 25;
+var number2 = '20.5';
+number2 = parseFloat(number2)
+// number2 = parseInt(number2)
+number2 = +number2;
+number1 = '25';
+number1 = parseFloat(number1)
+
+console.log(number1 + number2);
+var number3 = 0.1;
+var number4 = 0.2;
+var total = (number3+number4);
+total = total.toFixed(1)
+console.log(total);
+console.log(typeof number1)
+
+
+
 
 
 
