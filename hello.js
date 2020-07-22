a1d98618cd5021b565e7b126265f0196e43bf48a
ashraf-git <aliashraf.hena@gmail.com>
2020-07-22 12:40:08 +0600

    variabe-added

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log(121);
+var bananaPrice = 23;
+console.log(bananaPrice);
+
+var electricityBill = 612;
+console.log(electricityBill);
+
+var rickshawBara = 40;
+console.log(rickshawBara);
