5cac798e348187cb459debbd500bbf5bceaf26b5
ashraf-git <aliashraf.hena@gmail.com>
2020-07-22 13:16:40 +0600

    string boolean numeric added

--- hello.js
+++ hello.js
@@ -2,7 +2,20 @@ var bananaPrice = 23;
 console.log(bananaPrice);
 
 var electricityBill = 612;
-console.log(electricityBill);
+console.log(typeof electricityBill);
 
 var rickshawBara = 40;
-console.log(rickshawBara);
+console.log(typeof rickshawBara);
+
+var name = "Kolimuddin Juarder";
+console.log(name);
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(typeof isRich);
+
+var isMarried = true;
+var isFlood = false;
+
