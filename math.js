9cfa4f879b557b6c9e117eef9f7c3adf2f73d70b
ashraf-git <aliashraf.hena@gmail.com>
2020-07-23 08:01:39 +0600

    round-floor-ceil-random

--- math.js
+++ math.js
@@ -3,3 +3,13 @@ var price2 = 'ali';
 
 var total = price2 + ' ' + price1;
 console.log(total)
+
+// var number = 5.76675;
+// var absoluteNumber = Math.abs(number);
+// console.log(absoluteNumber)
+
+var number = 5.20;
+var result = Math.floor(number)
+var result = Math.random()*10
+var result = Math.round(result)
+console.log(result)
